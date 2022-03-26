578dd10099c64cf88e25150cbb1176374a88a92f
“Siva <shivku.goku@gmail.com>
2022-03-27 02:09:47 +0530

    First commit

---
+++ src/main/kotlin/com/research/surveyer/SurveyerApplication.kt
@@ -0,0 +1,11 @@
+package com.research.surveyer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SurveyerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SurveyerApplication>(*args)
+}
---
+++ src/test/kotlin/com/research/surveyer/SurveyerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.research.surveyer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SurveyerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
