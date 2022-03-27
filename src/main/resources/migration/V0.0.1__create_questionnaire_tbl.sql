976dba11aa2d786bca5acccc9c2f990b66fe4b20
“Siva <shivku.goku@gmail.com>
2022-03-27 13:25:11 +0530

    Implemented tests for repository.

---
+++ src/main/resources/migration/V0.0.1__create_questionnaire_tbl.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS Questionnaire (
+    id int(10) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    status varchar(255),
+    title varchar(255)
+)
---
+++ src/test/kotlin/com/research/surveyor/repositories/QuestionnaireRepositoryTest.kt
@@ -0,0 +1,35 @@
+package com.research.surveyor.repositories
+
+import com.research.surveyor.models.Questionnaire
+import com.research.surveyor.models.QuestionnaireStatus
+import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.`should not be`
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.context.junit4.SpringRunner
+
+@ExtendWith(SpringExtension::class)
+@DataJpaTest
+class QuestionnaireRepositoryTest {
+
+    @Autowired
+    private lateinit var questionnaireRepository: QuestionnaireRepository
+
+    @Test
+    internal fun `should save the questionnaire`() {
+        val savedQuestionnaire = questionnaireRepository.save(fakeQuestionnaire)
+
+        savedQuestionnaire.id `should not be` 0
+
+        // TODO: Improve fetching. Can't expect to get the id as 1, unless the db is cleared.
+        savedQuestionnaire `should be equal to` fakeQuestionnaire.copy(id = 1)
+    }
+}
+
+private val fakeQuestionnaire =
+    Questionnaire(title = "New questionnaire - creation", status = QuestionnaireStatus.DRAFT)
