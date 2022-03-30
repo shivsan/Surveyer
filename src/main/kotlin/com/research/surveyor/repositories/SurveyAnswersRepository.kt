c48fb8ac82866e0f5e4dff53715a0735be828a2c
“Siva <shivku.goku@gmail.com>
2022-03-31 01:38:15 +0530

    Removed TODOs.

--- src/main/kotlin/com/research/surveyor/repositories/SurveyAnswersRepository.kt
+++ src/main/kotlin/com/research/surveyor/repositories/SurveyAnswersRepository.kt
@@ -4,6 +4,5 @@ import com.research.surveyor.models.SurveyAnswers
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
-// TODO: add integration test
 @Repository
 interface SurveyAnswersRepository: CrudRepository<SurveyAnswers, Long>
